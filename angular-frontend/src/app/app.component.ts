5625e79c57c5d701a682a311735d430affc38835
Daniel Battaglia <daniel.battaglia@baloise.ch>
2023-10-16 16:27:24 +0200

    Use Stompjs for /stocks and /button

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,28 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpService } from 'src/http.service';
+import * as Stomp from 'stompjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  stock : any = {
+export class AppComponent implements OnInit, OnDestroy {
+  stock : any = {}
+
+  webSocket?: WebSocket;
+  client?: Stomp.Client;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.openWebSocketConnection();
   }
 
-  private stockSocket: WebSocket
-  private buttonSocket: WebSocket
+  ngOnDestroy(): void {
+    this.closeWebSocketConnection();
+  }
 
-  constructor() {
-    this.stockSocket = new WebSocket("ws://localhost:8080/stocks")
-    this.buttonSocket = new WebSocket("ws://localhost:8080/button")
-    this.stockSocket.onmessage = (event) => {
-      this.stock = JSON.parse(event.data)
+  openWebSocketConnection() {
+    this.webSocket = new HttpService().getWebSocket();
+    this.client = Stomp.over(this.webSocket);
+    this.client.connect({}, () => {
+      this.client?.subscribe("/stocks", (payload) => {
+        this.updateStocks(JSON.parse(payload.body));
+      });
+    });
+  }
+
+  closeWebSocketConnection() {
+    if (this.client && this.webSocket) {
+      this.webSocket.close();
+      this.client.unsubscribe("/item-updates");
     }
+  }
+
+  updateStocks(stock: any) {
+    this.stock = stock
   }
 
   onBuyStock() {
     var payload= { user: "John Doe" }
-
-    this.buttonSocket.send(JSON.stringify(payload));
+    this.client?.send("/button", {}, JSON.stringify(payload));
   }
 }
