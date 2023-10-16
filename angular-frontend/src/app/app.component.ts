c898ad70ad2608460d69d5fc32ed185bef8c5fcb
Daniel Battaglia <daniel.battaglia@baloise.ch>
2023-10-16 14:36:11 +0200

    Link frontend with Spring service

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -6,10 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  stock : any = {
-    name: "Baloise",
-    price: 128.40,
-    increased: true,
-    icon: "https://dwglogo.com/wp-content/uploads/2017/10/1200px-Baloise_logo-1024x705.png"
-  }
+  stock : any = {}
+
+  private webSocket: WebSocket;
+
+  constructor() {
+    this.webSocket = new WebSocket('ws://localhost:8080/stocks');
+    this.webSocket.onmessage = (event) => {
+      this.stock = JSON.parse(event.data)
+    };
+  } 
 }
