b698b3142d74854ec5dfdc22adf55f8a81131cf2
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2023-10-16 13:37:31 +0200

    initial spring boot module

---
+++ spring-backend/src/test/java/com/baloise/collab/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baloise.collab.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
