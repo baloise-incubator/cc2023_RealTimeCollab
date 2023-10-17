33867678366febccab7726da4ae64f66c5a44b75
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2023-10-17 10:20:36 +0200

    add cors config

--- spring-backend/src/main/java/com/baloise/collab/springbackend/security/BasicSecurityConfig.java
+++ spring-backend/src/main/java/com/baloise/collab/springbackend/security/BasicSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -40,6 +42,27 @@ public class BasicSecurityConfig {
                 .build());
 
         return new InMemoryUserDetailsManager(userDetails.toList());
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:4200"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "PUT",
+                                "POST",
+                                "DELETE",
+                                "PATCH",
+                                "OPTIONS"
+                        );
+            }
+        };
     }
 
 }
