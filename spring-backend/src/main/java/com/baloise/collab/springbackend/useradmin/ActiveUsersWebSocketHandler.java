29e20efed4319b52d7d0047070b900fc979f8725
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2023-10-17 13:58:04 +0200

    adapt pattern from scheduled sending to active user information

--- spring-backend/src/main/java/com/baloise/collab/springbackend/useradmin/ActiveUsersWebSocketHandler.java
+++ spring-backend/src/main/java/com/baloise/collab/springbackend/useradmin/ActiveUsersWebSocketHandler.java
@@ -2,6 +2,7 @@ package com.baloise.collab.springbackend.useradmin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.Set;
@@ -10,9 +11,11 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class ActiveUsersWebSocketHandler {
 
-    @SendTo("/topic/activeUsers")
-    public Set<UserDTO> updateActiveUsers(Set<UserDTO> activeUsers) {
-        return activeUsers;
+    private final SimpMessagingTemplate messageSender;
+
+    public void updateActiveUsers(Set<UserDTO> activeUsers) {
+        messageSender.convertAndSend("/topic/activeUsers",
+                activeUsers);
     }
 
 }
