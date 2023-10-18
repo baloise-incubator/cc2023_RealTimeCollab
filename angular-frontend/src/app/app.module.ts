ae219fd80e668e8bc99a5fea6a9fa8d0d555f1cd
daniel-battaglia <daniel.battaglia@baloise.ch>
2023-10-18 10:38:28 +0200

    Extract user bar component

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 import { InventoryItemComponent } from './components/inventory-item/inventory-item.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { StockComponent } from './components/stock/stock.component';
+import { UserBarComponent } from './components/user-bar/user-bar.component';
 import { balIconAccount } from '@baloise/design-system-icons'
 
 @NgModule({
-  declarations: [AppComponent, CheckInComponent, InventoryComponent, InventoryItemComponent, ChatComponent, StockComponent],
+  declarations: [AppComponent, CheckInComponent, InventoryComponent, InventoryItemComponent, ChatComponent, StockComponent, UserBarComponent],
     imports: [
         BrowserModule,
         // Provide all components and value accessors to the app module.
---
+++ angular-frontend/src/app/components/user-bar/user-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input} from '@angular/core';
+
+import { User } from '../../../model';
+
+@Component({
+  selector: 'app-user-bar',
+  template: `
+    <bal-tag-group>
+      <bal-popover *ngFor="let user of users" arrow hover tooltip :active="isActive">
+        <bal-tag [color]="user.color" bal-popover-trigger aria-haspopup="true" aria-describedby="tooltip">
+          {{ user.name.at(0) }}
+        </bal-tag>
+        <bal-popover-content>{{ user.name }}</bal-popover-content>
+      </bal-popover>
+    </bal-tag-group>
+  `
+})
+export class UserBarComponent {
+
+  @Input() users!: User[]
+
+}
