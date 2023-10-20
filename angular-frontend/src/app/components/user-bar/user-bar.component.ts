610635e740c2c3b32015a24019a181b7915a8ca3
Bastian Schneider <bastian_schneider@ymail.com>
2023-10-20 10:50:22 +0200

    fix popup-message on user-bar

--- angular-frontend/src/app/components/user-bar/user-bar.component.ts
+++ angular-frontend/src/app/components/user-bar/user-bar.component.ts
@@ -6,7 +6,7 @@ import { User } from '../../../model';
   selector: 'app-user-bar',
   template: `
     <bal-tag-group>
-      <bal-popover *ngFor="let user of users" arrow hover tooltip :active="isActive">
+      <bal-popover *ngFor="let user of users" arrow hover tooltip :hover="hover">
         <bal-tag [color]="user.color" bal-popover-trigger aria-haspopup="true" aria-describedby="tooltip">
           {{ user.name.at(0) }}
         </bal-tag>
